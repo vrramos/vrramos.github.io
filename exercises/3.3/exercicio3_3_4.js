ea2c7b407ccd6ce4b7ea9afa04b616806f5983a0
Victor Felipe <vf.ramos7@gmail.com>
2019-09-28 17:20:50 -0300

    Finalizado o exercicio 4 do dia 3.3

--- exercises/3.3/exercicio3_3_4.js
+++ exercises/3.3/exercicio3_3_4.js
@@ -1,9 +1,13 @@
-function verificaCaracteres(nomes){    
-    nomeMaior = '';    
-    for (i of nomes){
-        return nomes[1].lenght;
-    } 
-}
-nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+function nomeMaior(listaNomes){
+    nomeMaior = listaNomes[0];
 
-console.log(verificaCaracteres(nomes));
+    for (i in listaNomes){
+        if (listaNomes[i].length > nomeMaior.length){
+           nomeMaior = listaNomes[i]
+        }
+    }
+    return nomeMaior;
+}
+
+let nomesAleatorios = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+console.log(nomeMaior(nomesAleatorios))
