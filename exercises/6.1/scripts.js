27e5e3423588a6e04a26721b2a4006f788a96387
Victor Felipe <vf.ramos7@gmail.com>
2019-10-15 10:13:04 -0300

    Adicionado o botao de enviar e de limpar o formulario no html... criado a funcao preventDefault no javascript

--- exercises/6.1/scripts.js
+++ exercises/6.1/scripts.js
@@ -1,18 +1,40 @@
 window.onload = function() {
-    
-    let tagOption, tagSelect;
-    let i = 0;
-    let estados = [
-        "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", 
-        "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", 
-        "RO", "RS", "RR", "SC", "SE", "SP", "TO"
-    ]   
-    
-    tagSelect = document.querySelector('#estados');
+    function enviarFormulario() {
+        let botaoFormulario
 
-    for(i = 0; i < estados.length; i++){        
-        tagOption = document.createElement('option');
-        tagSelect.appendChild(tagOption);
-        tagOption.textContent = estados[i]
-    }    
+        botaoFormulario = document.querySelector('#button-submit')
+        botaoFormulario.addEventListener('click', function(event){
+            event.preventDefault()
+
+            validarData()
+
+        })
+    }
+
+    function validarData() {
+        let dataInformada
+
+        dataInformada = document.querySelector('#data-de-inicio').value
+        console.log(dataInformada)
+    }
+
+    function inserirEstados() {
+        let tagOption, tagSelect;
+        let i = 0;
+        let estados = [
+            "AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", 
+            "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", 
+            "RO", "RS", "RR", "SC", "SE", "SP", "TO"
+        ]   
+        
+        tagSelect = document.querySelector('#estados');
+        
+        for(i = 0; i < estados.length; i++){        
+            tagOption = document.createElement('option');
+            tagSelect.appendChild(tagOption);
+            tagOption.textContent = estados[i]
+        }    
+    }
+    inserirEstados()
+    enviarFormulario()
 }
