25f829845d5d625a6147bf97aeb00653fc31a140
Victor Felipe <vf.ramos7@gmail.com>
2019-10-18 09:21:41 -0300

    Adicionado a funcionalidade de validacao de data via Pikaday

--- exercises/6.2/scripts.js
+++ exercises/6.2/scripts.js
@@ -1,16 +1,17 @@
 window.onload = function() {
     function enviarFormulario() {
         let botaoFormulario
-
+        
         botaoFormulario = document.querySelector('#button-submit')
         botaoFormulario.addEventListener('click', function(event){
-       
+            
             event.preventDefault()
             pegarResultados()
             inserirResultadosNaDiv()
+            
         })
     }
-
+    
     function inserirEstados() {
         let tagOption, tagSelect;
         let i = 0;
@@ -28,14 +29,14 @@ window.onload = function() {
             tagOption.textContent = estados[i]
         }    
     }
-
+    
     function criaUmaDivParaInserirOsResultados() {
         let divResultados, elementoPai
         divResultados = document.createElement('div')
         elementoPai = document.getElementsByTagName('body')[0].appendChild(divResultados)
         return divResultados
     }
-
+    
     function pegarResultados() {
         let nome, email, cpf, endereco, cidade, selectEstado, estado, complemento
         let informacoes = []
@@ -46,24 +47,31 @@ window.onload = function() {
         endereco = document.querySelector('#endereco').value
         cidade = document.querySelector('#cidade').value
         selectEstado = document.querySelector('#estados').value
-        estado = selectEstado.options[selectEstado.selectedIndex].text
-
+        
         return informacoes = [nome,email,cpf,endereco,cidade,estado]
     }
-
+    
     function inserirResultadosNaDiv() {
         let i, span
-
+        
         for(i = 0; i < pegarResultados().length; i++){
             span = document.createElement('span')
             criaUmaDivParaInserirOsResultados().appendChild(span)
             span.textContent = pegarResultados()[i]
         }
     }
-
-
-
+    
+    function validarData() {
+        let picker = new Pikaday({ 
+            field: document.getElementById('data-de-inicio'),
+            format: 'D MMM YYYY'
+        });
+    }
+    
+    
+    
     inserirEstados()
     enviarFormulario()
     criaUmaDivParaInserirOsResultados()
+    validarData()
 }
