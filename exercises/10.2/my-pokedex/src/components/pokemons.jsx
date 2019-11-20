ebbe5c87a19c25a20f70e4e1bbde2c533c0f340a
Victor Felipe <vf.ramos7@gmail.com>
2019-11-20 09:26:47 -0300

    Finalizado o exercicio My Pokedex

---
+++ exercises/10.2/my-pokedex/src/components/pokemons.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Pokemons extends React.Component {
+    render() {
+        const { name, type, averageWeight, image, moreInfo } = this.props.pokemon;
+        return (
+            <div className="pokemon">
+                <div>
+                    <p>{name}</p>
+                    <p>{type}</p>
+                    <p> {`Average Weight: ${averageWeight.value} ${averageWeight.measurementUnit}`} </p>
+                    <p><a href={`${moreInfo}`}>More Info</a></p>
+                </div>
+                <img src={image} alt={`Pokemon ${name} Image.`} />
+            </div>
+        )
+    }
+}
+
+export default Pokemons;
